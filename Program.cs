1b7d31c8ee7dd4a2b1d54463e3602025c175d2ec
Russell Wallace <russell.wallace@gmail.com>
2024-04-30 13:50:13 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace check_file_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
